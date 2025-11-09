3ebb6fb4f0d5a2466893f379ea65f2c706db809c
Viren Lahane <virenlahane7@gmail.com>
2025-11-10 01:36:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
+
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that helps generate structured research responses.
+            Answer the user query and use necessary tools.
+            Output ONLY in this format:
+            {format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+tools=[search_tool, wiki_tool, save_tool]
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query=input("What can i help you research?")
+raw_response = agent_executor.invoke({"query": query})
+print("\nRaw response:", raw_response)
+
+try:
+    structured_response = parser.parse(raw_response["output"])
+    print("\nParsed structured output:\n", structured_response)
+except Exception as e:
+    print("\nError parsing response:", e)
+    print("Raw Response:", raw_response)
